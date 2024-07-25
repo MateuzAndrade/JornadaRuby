e3e70891c37a2edf4c53668743a8739f97ec9dd4
Mateus Andrade <mateus.nascimento.andrade@gmail.com>
2024-07-25 07:06:23 -0300

    Reinicio das pesquisas

--- Rails/PesquisaDeEngajamento/app/models/search.rb
+++
@@ -1,5 +0,0 @@
-class Search < ApplicationRecord
-  validates :name, presence: true
-  validates :active, inclusion: {in: [true,false]}
-  validates :year, presence: true, numericality: {only_integer: true}
-end
--- Rails/PesquisaDeEngajamento/config/routes.rb
+++ Rails/PesquisaDeEngajamento/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :departments
-  resources :managers
-
+  resources :manage
   root to: "welcome#index"
-  get 'inicio', to:'welcome#index'
+  get "inicio", to: "welcome#index"
 end
